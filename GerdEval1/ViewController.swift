07d693973bba45e89fd854703ceeb09c340037d6
Producer <producer@Maurices-iMac-2.local>
2017-03-06 15:34:29 -0800

    Initial Commit

---
+++ GerdEval1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GerdEval1
+//
+//  Created by Producer on 3/6/17.
+//  Copyright © 2017 Producer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
